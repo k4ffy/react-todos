abc2a5c3e784bda03415df56c02a8dd33be7eeca
Pavel <schabalov.pasha3@gmail.com>
2022-11-24 01:06:12 +0300

    Add form handle submit new todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/actions/todoActions';
 
 const Form = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [descr, setDescr] = useState('');
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (title.trim().length > 0) {
+      dispatch(addTodo({ title, descr }));
+
+      setTitle('');
+      setDescr('');
+    }
+  };
+
   return (
-    <form className="todo__form todo-form">
+    <form onSubmit={handleSubmitForm} className="todo__form todo-form">
       <div className="todo-form__input-wrapper">
         <input
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
           required
           type="text"
-          name="todo-title"
           className="todo-form__input-title"
           placeholder="Task name here..."
         />
         <textarea
-          name="todo-descr"
+          onChange={(e) => setDescr(e.target.value)}
+          value={descr}
           className="todo-form__input-descr"
           placeholder="Description"
         />
